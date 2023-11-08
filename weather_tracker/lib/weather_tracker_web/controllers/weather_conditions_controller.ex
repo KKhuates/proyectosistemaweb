485b39983e6747e39a31cf0ec25c3b31b9b12084
KKhuates <salvadortes05@gmail.com>
2023-11-07 22:30:48 -0300

    upload

--- weather_tracker/lib/weather_tracker_web/controllers/weather_conditions_controller.ex
+++ weather_tracker/lib/weather_tracker_web/controllers/weather_conditions_controller.ex
@@ -20,7 +20,7 @@ defmodule WeatherTrackerWeb.WeatherConditionsController do
         |> json(weather_condition)
 
       error ->
-        Logger.warn("Failed to create a weather entry: #{inspect(error)}")
+        Logger.warning("Failed to create a weather entry: #{inspect(error)}")
 
         conn
         |> put_status(:unprocessable_entity)
